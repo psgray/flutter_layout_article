ccfc5dc51d81c7abbf14d3fe527c24d67ebd5c3a
Peter Gray <maps@iinet.net.au>
2022-03-22 21:28:10 +1100

    Upgraded to Flutter 1.12+
    Upgraded to Flutter 1.12+ as per
    https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects.
    Builds and runs in Flutter 2.10.3

--- android/app/src/main/kotlin/com/example/flutter_layout_article/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_layout_article/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.flutter_layout_article
 
-import android.os.Bundle
+// Upgrading pre 1.12 android projects
+// See: https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+//import android.os.Bundle
+
+//import io.flutter.app.FlutterActivity
+//import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+//  override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    GeneratedPluginRegistrant.registerWith(this)
+//  }
 }
